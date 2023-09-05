edebd6363c1183fee0fd2a7d00bbe5d9d78e3120
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-09-05 17:00:23 -0300

    Nuevo componente responsive creado LowPrices

--- src/pages/inicio/index.tsx
+++ src/pages/inicio/index.tsx
@@ -1,12 +1,18 @@
-import Layout from '@/components/Layout'
-import React from 'react'
+import Layout from "@/components/Layout";
+import React from "react";
+import styles from "@/styles/Inicio.module.css";
+import LowPrices from "@/components/LowPrices";
 
 const Inicio = () => {
   return (
     <Layout>
-      Inicio
+      <div className="page">
+        <main className={styles.inicio}>
+          <LowPrices />
+        </main>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Inicio
+export default Inicio;
