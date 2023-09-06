b25ba2f2aa74bf6a6bfe7d04ffe1e92ab910ed07
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-09-06 11:19:59 -0300

    Creado Footer Responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import { Roboto } from "next/font/google";
 import Head from "next/head";
 import { ILayout } from "@/libs/interfaces";
 import NavigationBar from "./NavigationBar";
+import Footer from "./Footer";
 
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
@@ -16,10 +17,13 @@ const Layout: React.FC<ILayout> = ({ children }) => {
         <title>FlyLibo</title>
       </Head>
       <div style={roboto.style}>
-        <header style={{width: "100%", boxShadow: "1px 2px 6px gainsboro"}}>
+        <header style={{ width: "100%", boxShadow: "1px 2px 6px gainsboro" }}>
           <NavigationBar />
         </header>
         {children}
+        <footer style={{ width: "100%", backgroundColor: "#151515" }}>
+          <Footer />
+        </footer>
       </div>
     </>
   );
