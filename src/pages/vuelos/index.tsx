3f0da0fd8e2cac0eeb4f49bbb8a724983d48c5b9
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-09-14 16:41:01 -0300

    Vuelos route en proceso

--- src/pages/vuelos/index.tsx
+++ src/pages/vuelos/index.tsx
@@ -1,10 +1,19 @@
-import Layout from '@/components/Layout'
-import React from 'react'
+import Layout from "@/components/Layout";
+import { NextRouter, useRouter } from "next/router";
+import React, { useEffect } from "react";
+import styles from "@/styles/Vuelos.module.css";
 
 const Vuelos = () => {
-  return (
-    <Layout>Vuelos</Layout>
-  )
-}
+  const router: NextRouter = useRouter();
+  let origin = router.query.origin?.toString();
+  let destination = router.query.destination?.toString();
+  let passengers = router.query.passengers?.toString();
 
-export default Vuelos
+  return (
+    <Layout>
+      <div className={styles.vuelos}>Vuelos</div>
+    </Layout>
+  );
+};
+
+export default Vuelos;
