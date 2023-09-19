bcb4fe298c89366d8291f6696a5096b8d0d1129c
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-09-19 15:23:11 -0300

    TableFly y Ticket componentes finalizados. vuelos route finalizada

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -1,13 +1,60 @@
-import { IFly } from '@/libs/interfaces'
-import React from 'react'
-import styles from '@/styles/Components.module.css'
+import { IFly } from "@/libs/interfaces";
+import React, { useContext } from "react";
+import styles from "@/styles/Components.module.css";
+import Image from "next/image";
+import { FlyContext } from "@/context/FlyContext";
 
-const Ticket: React.FC<IFly> = ( props ) => {
+const Ticket: React.FC<IFly> = (props) => {
   return (
-    <div>
-      
+    <div className={styles.ticket}>
+      <div className={styles.ticket_top}>
+        <Image
+          className={styles.ticket_logo}
+          src={"/img/FlyLibo-logos_noback.png"}
+          alt="FlyLibo"
+          width={781}
+          height={319}
+        />
+      </div>
+      <div className={styles.ticket_mid}>
+        <Image
+          className={styles.ticket_midBackground}
+          src={
+            "https://res.cloudinary.com/dsuydyqgz/image/upload/v1695131810/01-varios/aarowfqs8bhiqkrzepsm.png"
+          }
+          alt="mapamundi"
+          width={413}
+          height={200}
+        />
+        <div className={styles.ticket_info}>
+          <p>
+            <b>Name:</b> Jhon Doe
+          </p>
+          <p>
+            <b>Asiento:</b> xyz
+          </p>
+        </div>
+        <div className={styles.ticket_or}>
+          <p>{props.origin}</p>
+          <Image
+            src={"/img/ticket_airplane.svg"}
+            alt="airplane"
+            width={60}
+            height={60}
+          />
+          <p>{props.destination}</p>
+        </div>
+        <div className={styles.ticket_boarding}>
+          <p className={styles.ticket_boardingTime}>Boarding Time</p>
+          <p>{Math.floor(Math.random() * 24) + 1}:00</p>
+        </div>
+      </div>
+      <div className={styles.ticket_bottom}>
+        <p>BOARDING PASS</p>
+        <p>Ticket de pasajero y equipaje</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Ticket
+export default Ticket;
--- src/pages/vuelos/index.tsx
+++ src/pages/vuelos/index.tsx
@@ -1,6 +1,6 @@
 import Layout from "@/components/Layout";
 import { NextRouter, useRouter } from "next/router";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import styles from "@/styles/Vuelos.module.css";
 import Image from "next/image";
 import data from "../../../FlyLiboDB.json";
@@ -17,27 +17,42 @@ const Vuelos = () => {
     <Layout>
       <div className={styles.vuelos}>
         <div className={styles.vuelos_container}>
-          <div className={styles.vuelos_subtitle}>
-            <p>Elegi tu vuelo de {originTicket === null ? "ida" : "vuelta"}</p>
-          </div>
-          <div className={styles.vuelos_title}>
-            <p>{originTicket === null ? origin : destination}</p>
-            <Image
-              src={"/img/arrow_right.svg"}
-              alt="arrow"
-              width={30}
-              height={30}
-            />
-            <p>{originTicket === null ? destination : origin}</p>
-          </div>
-          <div>
-            {originTicket !== null && <Ticket {...originTicket} />}
-            {destinationTicket !== null && <Ticket {...destinationTicket} />}
-          </div>
+          {originTicket === null || destinationTicket === null ? (
+            <>
+              <div className={styles.vuelos_subtitle}>
+                <p>
+                  Elegi tu vuelo de {originTicket === null ? "ida" : "vuelta"}
+                </p>
+              </div>
+              <div className={styles.vuelos_title}>
+                <p>{originTicket === null ? origin : destination}</p>
+                <Image
+                  src={"/img/arrow_right.svg"}
+                  alt="arrow"
+                  width={30}
+                  height={30}
+                />
+                <p>{originTicket === null ? destination : origin}</p>
+              </div>
+            </>
+          ) : undefined}
+          {originTicket !== null || destinationTicket !== null ? (
+            <div className={styles.vuelos_tickets}>
+              {originTicket !== null && <Ticket {...originTicket} />}
+              {destinationTicket !== null && <Ticket {...destinationTicket} />}
+            </div>
+          ) : undefined}
         </div>
-        <div className={styles.vuelos_tableContainer}>
-          <TableFly vuelos={data} />
-        </div>
+        {originTicket === null || destinationTicket === null ? (
+          <div className={styles.vuelos_tableContainer}>
+            <TableFly vuelos={data} />
+          </div>
+        ) : undefined}
+        {originTicket !== null && destinationTicket !== null ? (
+          <div className={styles.vuelos_buttonContainer}>
+            <button className={styles.vuelos_button}>comprar</button>
+          </div>
+        ) : undefined}
       </div>
     </Layout>
   );
