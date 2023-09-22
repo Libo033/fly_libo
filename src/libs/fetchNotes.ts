ad710db134d6c8808dea1135ed8de379f433d495
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-09-22 11:55:20 -0300

    Creado fetchNotes-hook y createNote-fucntion para usar la db de FireBase

---
+++ src/libs/fetchNotes.ts
@@ -0,0 +1,54 @@
+import { AuthContext } from "@/context/AuthContext";
+import {
+  DocumentData,
+  DocumentReference,
+  DocumentSnapshot,
+  doc,
+  getDoc,
+} from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { db } from "./Firebase";
+
+const useFetchNotes = (): {
+  loaded: boolean;
+  fetchNotes: DocumentData | null;
+} => {
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const [fetchNotes, setFetchNotes] = useState<DocumentData | null>(null);
+  const { user, loaded: authLoaded } = useContext(AuthContext);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (authLoaded) {
+        try {
+          if (user === null) {
+            throw new Error("User isn't loaded!");
+          }
+          const docRef: DocumentReference<DocumentData> = doc(
+            db,
+            "users",
+            user.uid
+          );
+          const docSnap: DocumentSnapshot<DocumentData> = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+            setFetchNotes(docSnap.data());
+          } else {
+            console.log("Ops");
+          }
+        } catch (error) {
+          if (error instanceof Error) {
+            console.log(error.message);
+          }
+        } finally {
+          setLoaded(false);
+        }
+      }
+    }
+    fetchData();
+  }, [user]);
+
+  return { loaded, fetchNotes };
+};
+
+export default useFetchNotes;
